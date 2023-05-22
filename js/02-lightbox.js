02b423f21b390268e3b3e0470a381f5bbe3c0df3
Vasylysaskyba <vasylysaskyba@gmail.com>
2023-05-22 22:53:59 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,14 +3,10 @@ import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
 
-
-
 const galleryContainer = document.querySelector(".gallery");
 
 const imagesMarkup = createImagesMarkup(galleryItems);
 galleryContainer.insertAdjacentHTML("beforeend", imagesMarkup);
-
-galleryContainer.addEventListener("click", onImagesContainerClick);
 
 function createImagesMarkup(galleryItems) {
   const markup = galleryItems
@@ -34,33 +30,3 @@ const lightbox = new SimpleLightbox(".gallery a", {
   captionDelay: 250,
   captionsData: "alt",
 });
-
-function onImagesContainerClick(event) {
-  event.preventDefault();
-  if (event.target.nodeName !== "IMG") {
-    return;
-  }
-
-  const selectedImage = event.target.dataset.source;
-
-  const instance = basicLightbox.create(
-    `<img src = "${selectedImage}" width = "600" height = "400">`,
-    {
-      onShow: (instance) => {
-        window.addEventListener("keydown", onEscKeyPress);
-      },
-
-      onClose: (instance) => {
-        window.removeEventListener("keydown", onEscKeyPress);
-      },
-    }
-  );
-
-  instance.show();
-
-  function onEscKeyPress(e) {
-    if (e.code !== "Escape") return;
-
-    instance.close();
-  }
-}
